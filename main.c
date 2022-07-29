654aa08d5e568acddf56f5663a146fd6f0aa8416
Fatih Koçer <fatihkocerc@gmail.com>
2022-07-29 19:01:55 +0300

    update main.c

--- main.c
+++ main.c
@@ -19,34 +19,29 @@ void test()
 	hash_table *table = create_table(TABLE_SIZE);
 
 	//add data via key
-	add_entry(table, TABLE_SIZE, "key1", KEY_SIZE, "data1", DATA_SIZE,
-		  &cmp);
-	add_entry(table, TABLE_SIZE, "key2", KEY_SIZE, "data2", DATA_SIZE,
-		  &cmp);
-	add_entry(table, TABLE_SIZE, "key3", KEY_SIZE, "data3", DATA_SIZE,
-		  &cmp);
-	add_entry(table, TABLE_SIZE, "key4", KEY_SIZE, "data4", DATA_SIZE,
-		  &cmp);
-	add_entry(table, TABLE_SIZE, "key5", KEY_SIZE, "data5", DATA_SIZE,
-		  &cmp);
+	add_entry(table, TABLE_SIZE, "key1", KEY_SIZE, "data1", DATA_SIZE, &cmp);
+	add_entry(table, TABLE_SIZE, "key2", KEY_SIZE, "data2", DATA_SIZE, &cmp);
+	add_entry(table, TABLE_SIZE, "key3", KEY_SIZE, "data3", DATA_SIZE, &cmp);
+	add_entry(table, TABLE_SIZE, "key4", KEY_SIZE, "data4", DATA_SIZE, &cmp);
+	add_entry(table, TABLE_SIZE, "key5", KEY_SIZE, "data5", DATA_SIZE, &cmp);
 
 	//search entries
 	hash_table *temp;
-	temp = search_entry(table, TABLE_SIZE, "key1", KEY_SIZE);
-	printf("key=%s\tdata=%s\n", (char *)temp->value->key,
-	       (char *)temp->value->data);
+	
+        temp = search_entry(table, TABLE_SIZE, "key1", KEY_SIZE);
+	printf("key=%s\tdata=%s\n", (char *)temp->value->key, (char *)temp->value->data);
+
 	temp = search_entry(table, TABLE_SIZE, "key2", KEY_SIZE);
-	printf("key=%s\tdata=%s\n", (char *)temp->value->key,
-	       (char *)temp->value->data);
-	temp = search_entry(table, TABLE_SIZE, "key3", KEY_SIZE);
-	printf("key=%s\tdata=%s\n", (char *)temp->value->key,
-	       (char *)temp->value->data);
-	temp = search_entry(table, TABLE_SIZE, "key4", KEY_SIZE);
-	printf("key=%s\tdata=%s\n", (char *)temp->value->key,
-	       (char *)temp->value->data);
-	temp = search_entry(table, TABLE_SIZE, "key5", KEY_SIZE);
-	printf("key=%s\tdata=%s\n", (char *)temp->value->key,
-	       (char *)temp->value->data);
+	printf("key=%s\tdata=%s\n", (char *)temp->value->key, (char *)temp->value->data);
+	
+        temp = search_entry(table, TABLE_SIZE, "key3", KEY_SIZE);
+	printf("key=%s\tdata=%s\n", (char *)temp->value->key, (char *)temp->value->data);
+	
+        temp = search_entry(table, TABLE_SIZE, "key4", KEY_SIZE);
+	printf("key=%s\tdata=%s\n", (char *)temp->value->key, (char *)temp->value->data);
+	
+        temp = search_entry(table, TABLE_SIZE, "key5", KEY_SIZE);
+	printf("key=%s\tdata=%s\n", (char *)temp->value->key, (char *)temp->value->data);
 
 	//delete entries
 	del_entry(table, TABLE_SIZE, "key1", KEY_SIZE);
